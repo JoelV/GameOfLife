4594a7da040d5d34e58593745ffe30b71ab9c881
JoelV <joelv@joelv-HP-Compaq-6530b-NA407UC-ABA.(none)>
2013-08-15 14:37:47 -0400

    Implemented tick

--- life.js
+++ life.js
@@ -18,7 +18,7 @@ function GameOfLife() {
     	var str = ""
 		_.each(this.currentState, function(i) {
 			_.each(i, function(j) {
-				str += j;
+				str += " " + j;
 			});
 			str += "\n";
 		});
@@ -34,6 +34,7 @@ function GameOfLife() {
 				this.currentState[i][j] = 0;
 			}
 		}
+		
 		//this.printArray(this.currentState);
 	}
 	this.addAlive = function(x, y) {
@@ -41,23 +42,51 @@ function GameOfLife() {
 	}
 	this.numOfAliveNeighbors = function(x, y) {
 		var count = 0;
-		if(this.currentState[x-1][y-1] === 1) count++;
-		if(this.currentState[x-1][y] === 1) count++;
-		if(this.currentState[x-1][y+1] === 1) count++;
-		if(this.currentState[x-1][y] === 1) count++;
-		if(this.currentState[x+1][y] === 1) count++;
-		if(this.currentState[x-1][y+1] === 1) count++;
-		if(this.currentState[x][y+1] === 1) count++;
-		if(this.currentState[x+1][y+1] === 1) count++;
+		//console.log(x);
+		if((x !== 0 && y !== 0)&& this.currentState[x-1][y-1] === 1) count++;
+		if(y !== 0 && this.currentState[x][y-1] === 1) count++;
+		if((x < this.currentState.length - 1 && y !== 0) && this.currentState[x+1][y-1] === 1) count++;
+		if(x !== 0 && this.currentState[x-1][y] === 1) count++;
+		if(x < this.currentState.length - 1&& this.currentState[x+1][y] === 1) count++;
+		if((x != 0 && y < this.currentState[x].length) && this.currentState[x-1][y+1] === 1) count++;
+		if(y < this.currentState[x].length - 1 && this.currentState[x][y+1] === 1) count++;
+		if((x < this.currentState.length - 1 && y < this.currentState[x].length) && this.currentState[x+1][y+1] === 1) count++;
 		return count;
 	}
 	this.tick = function() {
+		var newState = [];
+		for(var i = 0; i < this.currentState.length; i++) {
+			newState[i] = [];
+			for(var j = 0; j < this.currentState[i].length; j++) {
+				newState[i][j] = 0;
+			}
+		}
+		_.each(this.currentState, function(arryI, i) {
+			_.each(arryI, function(arryIJ, j){
+				var aliveNeighbors = g1.numOfAliveNeighbors(i, j);
+				if(arryIJ === 0 && aliveNeighbors === 3) newState[i][j] = 1;
+				else if(arryIJ === 1) {
+					if(aliveNeighbors > 3) newState[i][j] = 0;
+					else if(aliveNeighbors === 3 || aliveNeighbors === 2) newState[i][j] = 1;
+					else newState[i][j] = 0;
+				}
+			});
+		});
+		
+		this.currentState = newState;
 	}
 }
 
 var g1 = new GameOfLife();
 g1.init(10, 10);
+
 g1.addAlive(5,5);
 g1.addAlive(5,6);
 g1.addAlive(6,5);
+g1.addAlive(4,5);
+//console.log(g1.numOfAliveNeighbors(5,5));
+g1.printState();
+g1.tick();
+g1.printState();
+g1.tick();
 g1.printState();
